aaa5c76f94915e4f517fe0a868c2e475355437eb
tsili852 <nick.tsilivis@gmail.com>
2014-03-22 19:29:36 +0200

    Main.fm check in

---
+++ ChScan/ChScan/Main.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Windows.Forms
+
+Public Class Main
+
+    Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'ScanBtn.ImageScaling = ToolStripItemImageScaling.None
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        End
+    End Sub
+
+    Private Sub quitBtn_Click(sender As Object, e As EventArgs) Handles quitBtn.Click
+        ExitToolStripMenuItem.PerformClick()
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        Dim aboutFrm As AboutFrm = New AboutFrm
+        aboutFrm.Owner = Me
+        aboutFrm.MdiParent = Me
+        aboutFrm.StartPosition = FormStartPosition.CenterScreen
+        aboutFrm.Show()
+    End Sub
+End Class
