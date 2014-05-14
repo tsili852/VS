2b666b0a99bf45194af317e4f94e8c4cfdcbbdb0
WorkMe <tsilivisn@unisystems.gr>
2014-05-14 15:33:58 +0300

    FrmScan First Build Just scan the schecks. No image desplayed

--- ChScan/ChScan/Main.vb
+++ ChScan/ChScan/Main.vb
@@ -1,8 +1,18 @@
 ﻿Imports System.Windows.Forms
 
-Public Class Main
+Public Class FrmMain
 
     Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim foo As MsgBoxResult
+        If Process.GetProcessesByName _
+          (Process.GetCurrentProcess.ProcessName).Length > 1 Then
+            foo = MsgBox("Application is already running", vbCritical)
+            End
+        End If
+        readerini = "C:\NDPWork\HSBC\SOURCE_NDP" + "\READER.INI"
+        endorseini = "C:\NDPWork\HSBC\SOURCE_NDP" + "\ENDORSE.INI"
+        carSetupini = "C:\NDPWork\HSBC\SOURCE_NDP" + "\IMAGE.INI"
+
         Dim currentWidth As Integer = Screen.PrimaryScreen.Bounds.Width
         Dim currentHeight As Integer = Screen.PrimaryScreen.Bounds.Height
         If currentWidth < 1280 Then
@@ -21,7 +31,7 @@ Public Class Main
         End
     End Sub
 
-    Private Sub quitBtn_Click(sender As Object, e As EventArgs) Handles quitBtn.Click
+    Private Sub quitBtn_Click(sender As Object, e As EventArgs)
         ExitToolStripMenuItem.PerformClick()
     End Sub
 
@@ -31,5 +41,17 @@ Public Class Main
         aboutFrm.MdiParent = Me
         aboutFrm.StartPosition = FormStartPosition.CenterScreen
         aboutFrm.Show()
+    End Sub
+
+    Private Sub OrdinaryChecksToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles OrdinaryChecksToolStripMenuItem1.Click
+        FrmScan.Show()
+    End Sub
+
+    Private Sub TSBtnExit_Click(sender As Object, e As EventArgs) Handles TSBtnExit.Click
+        End
+    End Sub
+
+    Private Sub TSBtnScan_Click(sender As Object, e As EventArgs) Handles TSBtnScan.Click
+        OrdinaryChecksToolStripMenuItem1.PerformClick()
     End Sub
 End Class
